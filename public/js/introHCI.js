b16ea52fd3e2b1d7e290541efacfdd560aa5a488
UUUZ <chuyuzi428@gmail.com>
2021-02-18 11:50:55 -0800

    updated

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(clickLikeBtn);
+}
+
+function clickLikeBtn(e){
+	e.preventDefault();
+	ga('create','G-VV9QH67HYJ','auto')
+	ga("send", "event", 'like', 'click');
 }
